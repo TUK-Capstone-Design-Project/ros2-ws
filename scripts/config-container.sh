2ac3522625579017c85d341114c4fca925d900d8
hanch2396 <hanch2396@gmail.com>
2026-05-11 22:38:06 +0900

    이미지 업데이트

--- scripts/config-container.sh
+++ scripts/config-container.sh
@@ -5,7 +5,7 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # 이미지 및 컨테이너 이름 설정
 IMAGE_NAME="ros2-sdk"
-IMAGE_TAG="26.5.9"
+IMAGE_TAG="26.5.11"
 CONTAINER_NAME="ros2-workspace"
 
 # 원격 설정 (Docker Hub 또는 개인 레지스트리)
